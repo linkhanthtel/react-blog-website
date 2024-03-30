76314a9f5f94c8235ff44b73b11b32fd7250ee63
Lin Khant Htel <linkhanthtel@icloud.com>
2024-03-30 16:53:34 +0800

    UI Updated

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -5,10 +5,10 @@ import image3 from '../images/image3.jpg'
 function Article({title}) {
   return (
     <div className='my-5 mx-3 flex flex-col md:flex-row p-5 bg-white shadow-lg shadow-gray-300 hover:shadow-gray-500'>
-      <div className='flex justify-center items-center'>
-        <img src={image3} alt="Image" className='w-56 h-48 md:w-[90%] md:h-84' />
+      <div className='flex justify-center items-center md:w-2/5'>
+        <img src={image3} alt="Image" className='w-72 h-48 md:w-[90%] md:h-84' />
       </div>
-      <div>
+      <div className='md:w-3/5'>
         <h1 className='px-5 py-3 text-center text-xl md:px-2'>{title}</h1>
         <p className='px-5 py-3 md:px-2 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque a amet atque quas alias et laboriosam quae minima rerum ipsum? Natus neque ex cumque recusandae pariatur possimus iste vero veritatis! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, laborum architecto nostrum qui sequi dolor magni repellendus laboriosam? Culpa reprehenderit eos eum repellat. Laboriosam dolorem saepe provident eaque earum expedita.</p>
         <div className='flex justify-between px-5 py-3 mb-3 text-sm md:px-2'>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaFacebook } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaYoutube } from "react-icons/fa";
 
 function Navbar() {
+  const [dropDown, setDropDown] = useState(false);
+
   return (
-    <div className='p-4 bg-blue-500 text-white flex justify-around'>
+    <div className='fixed w-full py-4 bg-blue-500 text-white flex justify-around'>
       <div>
         <Link to="/">WanderLuxe Ventures</Link>
       </div>
+
       <div className='hidden md:flex'>
         <Link to="/" className='px-3 hover:bg-white hover:text-blue-500'>Home</Link>
         <Link to="/blogs" className='px-3 hover:bg-white hover:text-blue-500'>Blogs</Link>
         <Link to="/join" className='px-3 hover:bg-white hover:text-blue-500'>Join</Link>
         <Link to="/contact" className='px-3 hover:bg-white hover:text-blue-500'>Contact</Link>
       </div>
+
       <div className='hidden md:flex'>
         <div className='flex px-4'>
           <div className='text-xl px-2'>
@@ -29,6 +33,23 @@ function Navbar() {
           </div>
         </div>
       </div>
+
+      <div className='flex md:hidden'>
+        <button onClick={() => setDropDown(prev => !prev)}>Button</button>
+      </div>
+
+      {dropDown ? (
+          <div className='z-10 absolute mt-20 w-full h-full flex flex-col px-7 py-7 bg-green-400 text-center'>
+            <button onClick={() => setDropDown(prev => !prev)}>Button</button>
+            <Link>Home</Link>
+            <Link>Blogs</Link>
+            <Link>Join</Link>
+            <Link>Contact</Link>
+          </div>
+      ) : (<span></span>)
+      }
+
+
     </div>
   )
 }
