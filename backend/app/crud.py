d981dfe5d837cb65c01c93676b8f4b160dac3c06
Lin Khant Htel <linkhanthtel@icloud.com>
2025-09-15 22:37:01 +0800

    Update crud.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -40,7 +40,7 @@ def get_posts_by_user(db: Session, user_id: int, skip: int = 0, limit: int = 10)
     return db.query(models.Post).filter(models.Post.owner_id == user_id).order_by(desc(models.Post.created_at)).offset(skip).limit(limit).all()
 
 def create_post(db: Session, post: schemas.PostCreate, user_id: int):
-    db_post = models.Post(**post.dict(), owner_id=user_id)
+    db_post = models.Post(**post.model_dump(), owner_id=user_id)
     db.add(db_post)
     db.commit()
     db.refresh(db_post)
@@ -49,7 +49,7 @@ def create_post(db: Session, post: schemas.PostCreate, user_id: int):
 def update_post(db: Session, post_id: int, post: schemas.PostUpdate, user_id: int):
     db_post = db.query(models.Post).filter(models.Post.id == post_id, models.Post.owner_id == user_id).first()
     if db_post:
-        update_data = post.dict(exclude_unset=True)
+        update_data = post.model_dump(exclude_unset=True)
         for key, value in update_data.items():
             setattr(db_post, key, value)
         db.commit()
